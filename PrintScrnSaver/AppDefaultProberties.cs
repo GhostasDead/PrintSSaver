93450efe7ee9f01f6577d9a82b3b79ee28a7e0bc
GhostasDead <ahmad.salahia@gmail.com>
2022-10-18 14:29:38 +0300

    a class for app's default proberties and settings.

---
+++ PrintScrnSaver/AppDefaultProberties.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrintSSaver
+{
+    static class AppDefaultProberties
+    {
+        #region Settings' path
+        /// <summary>
+        /// Gets a directory in %AppData%\ folder.
+        /// </summary>
+        public static string GetAppDir(string appName)
+        {
+            return $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\{appName}";
+        }
+
+        /// <summary>
+        /// Gets path of a file in a certain directory.
+        /// </summary>
+        public static string GetSettingsFile(string appDir, string settingsFile)
+        {
+            return $@"{appDir}\{settingsFile}";
+        }
+        #endregion
+
+        #region Settings' data
+        /// <summary>
+        /// App's specific default values.
+        /// </summary>
+        public static string[] DefaultDataValues()
+        {
+            string defaultLocation = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)}\\PrintSSaver";
+            string defaultName = "Title+Timestamp";
+            string defaultTitle = "ProcessName";
+            string defaultFormat = "png";
+            string defaultSFX = "SFXdisabled%DefaultSound";
+            return new string[] { defaultName, defaultName, defaultTitle, defaultFormat, defaultSFX };
+        }
+        #endregion
+    }
+}
