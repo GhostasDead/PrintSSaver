c262f791de331af97517d425ca1388cd7d436222
GhostasDead <ahmad.salahia@gmail.com>
2022-10-18 14:33:39 +0300

    increase app version and add description.

--- PrintScrnSaver/Properties/AssemblyInfo.cs
+++ PrintScrnSaver/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PrintSSaver by A.S. (GhostasDead)")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("App to take screenshots and save clipboard images.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PrintSSaver")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
